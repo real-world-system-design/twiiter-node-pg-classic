417a4d5c4143d3d6f0a72bc298ffcc7e069545c0
Rahul kumar <rahulparida933@gamil.com>
2021-06-24 09:29:17 +0530

    endpoints for user controller done

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get('/@:username')
-  getUserByUsername(@Param() param): string {
-    return `details of username ${param.username}`;
+  getUserByUsername(@Param('username') username: string): string {
+    return `details of username ${username}`;
+  }
+
+  @Get('/:userid')
+  getUserById(@Param('userid') userid: string): string {
+    return `user by id ${userid}`;
+  }
+
+  @Post('/')
+  createNewUser(): string {
+    return 'new user created';
+  }
+
+  @Patch('/:userid')
+  updateUserDetails(@Param('userid') userid: string) {
+    return `initiate a update request for user ${userid}`;
+  }
+
+  @Delete('/:userid')
+  delteUserById(@Param('userid') userid: string) {
+    return `initiate a delete request for user ${userid}`;
   }
 }
